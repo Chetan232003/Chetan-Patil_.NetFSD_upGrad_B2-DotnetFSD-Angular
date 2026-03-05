2648d6e749d6540e8fc94026dfbb6e09f66460a3
Chetan Patil <116863948+Chetan232003@users.noreply.github.com>
2026-03-05 12:36:45 +0530

    Store Wise Sales Summary.sql

--- Week 3/Day 3 Handson/Store Wise Sales Summary.sql
+++ Week 3/Day 3 Handson/Store Wise Sales Summary.sql
@@ -1,9 +1,10 @@
 SELECT 
 s.store_name,
 SUM(oi.quantity * oi.list_price * (1 - oi.discount)) AS total_sales
-FROM stores s, orders o, order_items oi
+FROM stores s, orders1 o, order_items oi
 WHERE s.store_id = o.store_id
 AND o.order_id = oi.order_id
 AND o.order_status = 4
 GROUP BY s.store_name
+
 ORDER BY total_sales DESC;
