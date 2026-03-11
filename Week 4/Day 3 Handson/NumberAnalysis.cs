7012cb88c9102ea1293f8bb6e60e7395b2992787
Chetan Patil <116863948+Chetan232003@users.noreply.github.com>
2026-03-11 23:20:47 +0530

    Add files via upload

---
+++ Week 4/Day 3 Handson/NumberAnalysis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class NumberAnalysis
+{
+    static void Main()
+    {
+        Console.Write("Enter Number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int evenCount = 0;
+        int oddCount = 0;
+        int sum = 0;
+
+        // Loop from 1 to N
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+
+            if (i % 2 == 0)
+            {
+                evenCount++;
+            }
+            else
+            {
+                oddCount++;
+            }
+        }
+
+        Console.WriteLine("Even Count: " + evenCount);
+        Console.WriteLine("Odd Count: " + oddCount);
+        Console.WriteLine("Sum: " + sum);
+    }
+}
