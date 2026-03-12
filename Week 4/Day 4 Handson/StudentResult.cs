2ccf91e06fa773c63c14b36193bd41fed91cbb99
Chetan Patil <116863948+Chetan232003@users.noreply.github.com>
2026-03-12 17:32:23 +0530

    Add files via upload

---
+++ Week 4/Day 4 Handson/StudentResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Day_4_Handson
+{
+    internal class StudentResult
+    {
+        public void CalculateResult(int m1,int m2,int m3 ,out int total , out double average) 
+        {
+             total = m1 + m2 + m3;
+            average = total / 3;
+            
+        }
+
+        public static void Main(string[] args) 
+        {
+            StudentResult result = new StudentResult();
+
+            Console.WriteLine("Enter marks of 3 subjects:");
+
+            int m1 = Convert.ToInt32(Console.ReadLine());
+            int m2 = Convert.ToInt32(Console.ReadLine());
+            int m3 = Convert.ToInt32(Console.ReadLine());
+
+            int total;
+            double average;
+
+            
+            result.CalculateResult(m1, m2, m3, out total, out average);
+
+            Console.WriteLine("Total Marks: " + total);
+            Console.WriteLine("Average Marks: " + average);
+
+            if (average >= 40)
+                Console.WriteLine("Result: Pass");
+            else
+                Console.WriteLine("Result: Fail");
+
+
+        }
+    }
+}
