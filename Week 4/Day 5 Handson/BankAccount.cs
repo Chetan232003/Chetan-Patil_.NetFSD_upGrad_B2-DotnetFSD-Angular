849506e59ce78ed6e016b193f266d58b092b31a2
Chetan Patil <116863948+Chetan232003@users.noreply.github.com>
2026-03-16 12:02:59 +0530

    Add files via upload

---
+++ Week 4/Day 5 Handson/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount_Problem
+{
+    internal class BankAccount
+    {
+
+        private double balance;
+
+        public void deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine("Deposit balance : " + amount);
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Ammount");
+            }
+
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > 0)
+            {
+                balance -= amount;
+                Console.WriteLine("Withdraw Ammount : " + amount);
+            }
+            else
+            {
+                Console.WriteLine("Enter the Appropriate Ammount > 0");
+            }
+        }
+        public double getbalance()
+        {
+            return balance;
+        }
+
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.deposit(1000);
+            bankAccount.Withdraw(100);
+
+            Console.WriteLine("Current Balance :"+bankAccount.getbalance());
+        }
+    }
+}
