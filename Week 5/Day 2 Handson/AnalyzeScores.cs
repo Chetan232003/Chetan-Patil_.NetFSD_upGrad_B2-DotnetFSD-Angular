00c85527eea690e38c3317b1df898f9c6f9a9060
Chetan Patil <116863948+Chetan232003@users.noreply.github.com>
2026-04-03 09:22:55 +0530

    Add files via upload

---
+++ Week 5/Day 2 Handson/AnalyzeScores.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StudentAnalyzer
+{
+    public static void AnalyzeScores()
+    {
+        int[] marks = { 78, 85, 90, 67, 88 };
+        int threshold = 80;
+
+        int total = marks.Sum();
+
+        double average = marks.Average();
+
+        int highest = marks.Max();
+
+        var aboveThreshold = marks.Where(m => m > threshold).ToList();
+
+        Dictionary<string, int> subjectMarks = new Dictionary<string, int>()
+        {
+            { "Math", 90 },
+            { "Science", 88 },
+            { "English", 85 }
+        };
+
+     
+        Console.WriteLine("Total Marks: " + total);
+        Console.WriteLine("Average Marks: " + average);
+        Console.WriteLine("Students above " + threshold + ": " + aboveThreshold.Count);
+        Console.WriteLine("Highest Score: " + highest);
+
+        Console.WriteLine("\nSubject-wise Highest Marks:");
+        foreach (var subject in subjectMarks)
+        {
+            Console.WriteLine(subject.Key + ": " + subject.Value);
+        }
+    }
+}
---
+++ Week 5/Day 2 Handson/StackUndo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StackUndo
+{
+    private List<string> stack = new List<string>();
+
+    public void Push(string action)
+    {
+        stack.Add(action);
+        Console.WriteLine("Action Added: " + action);
+    }
+
+    public void Pop()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Nothing to undo!");
+            return;
+        }
+
+        string removed = stack[stack.Count - 1];
+        stack.RemoveAt(stack.Count - 1);
+        Console.WriteLine("Undo Action: " + removed);
+    }
+
+   
+    public bool IsEmpty()
+    {
+        return stack.Count == 0;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Current State: " + string.Join(", ", stack));
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        StackUndo editor = new StackUndo();
+
+        editor.Push("Type A");
+        editor.Push("Type B");
+        editor.Push("Type C");
+
+        editor.Pop(); // Undo
+        editor.Pop(); // Undo
+
+        editor.Display();
+    }
+}
+
